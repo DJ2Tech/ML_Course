37a0b0d7901f14dfe314c536460696bdf63621b1
DJ2Tech <Dj2tech@outlook.com>
2025-05-23 13:14:51 +0300

    Heater-Excess-Air-LR-Calculation.py

---
+++ 06.تعلم الآلة Machine Learning/Heater-Excess-Air-LR-Calculation.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+import copy
+import math
+
+
+data = np.loadtxt("data/EAir.txt", delimiter='\t')
+X = data[:,0]
+y = data[:,1]
+x_train=X
+y_train=y
+
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+  
+    
+
+    m = len(x)
+    
+    # An array to store cost J and w's at each iteration — primarily for graphing later
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b )  
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(x, y, w, b)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w, b, J_history, w_history 
+
+
+initial_w = 0.
+initial_b = 0.
+
+iterations = 300000
+alpha = 0.0001
+
+w,b,_,_ = gradient_descent(x_train ,y_train, initial_w, initial_b, 
+                     compute_cost, compute_gradient, alpha, iterations)
+print("w,b found by gradient descent:", w, b)
